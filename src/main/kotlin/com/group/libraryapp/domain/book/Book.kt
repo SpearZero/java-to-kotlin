7b78585b4bc45c58e312f76b6a28b90273393581
SpearZero <spearzero0@gmail.com>
2026-01-05 10:41:38 +0900

    첫 번째 요구사항 추가하기 - 책의 분야
    Boolean을 Enum으로 변경

--- src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
+++ src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
@@ -1,6 +1,8 @@
 package com.group.libraryapp.domain.book
 
 import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
@@ -9,6 +11,9 @@ import javax.persistence.Id
 class Book(
     val name: String,
 
+    @Enumerated(EnumType.STRING)
+    val type: BookType,
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
@@ -16,6 +21,20 @@ class Book(
     init {
         if (name.isBlank()) {
             throw IllegalArgumentException("이름은 비어 있을 수 없습니다")
+        }
+    }
+
+    companion object {
+        fun fixture(
+            name: String = "책 이름",
+            type: BookType = BookType.COMPUTER,
+            id: Long? = null
+        ): Book {
+            return Book(
+                name = name,
+                type = type,
+                id = id
+            )
         }
     }
 }
